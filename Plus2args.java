dda0cc3991f3048f1a89e9eab2d524ac02ead039
daivid-daisuke <dkawanam@gmail.com>
2019-08-20 14:37:27 +0800

    Plus2args.java
    pulling from https://qiita.com/Higemal/items/04f77b0e2aa55d9bcfa2

---
+++ Plus2args.java
@@ -0,0 +1,15 @@
+//  Plus2args
+
+public class Plus2args {
+        public static void main (String[] args){
+                add(args);
+        }
+
+        public static void add(final String[] args) {
+                int number;
+                number = Integer.parseInt(args[0]) + Integer.parseInt(args[1]);
+                System.out.println(number);
+                System.exit(0);
+        }
+}
+// EOL
